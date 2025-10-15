1e066a8d5ea28a423822d1104d66e5c296ace45c
finn.xu <ouhuangff@163.com>
2025-10-15 10:32:12 +0800

    🐳 chore:vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
   server: {
     port: 5173,
   },
